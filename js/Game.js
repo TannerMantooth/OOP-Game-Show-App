e24ff672b253e55074945304ee2212ecb5202546
TannerMantooth <137127335+TannerMantooth@users.noreply.github.com>
2023-09-20 19:10:38 -0500

    Setting up initial classes and values
    This update set up the main properties for the game and phrase class while adding empty methods that will be developed.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Finishing the fight sir'),
+            new Phrase('It is no sin to fight for what is right'),
+            new Phrase('Death is simply another stage of our life'),
+            new Phrase('I would rather be a brainless monkey than a heartless monster'),
+            new Phrase('You will laugh at your fears when you find out who you are')
+        ];
+        this.activePhrase = null;
+    }
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+        
+    }
+}
