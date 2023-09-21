207195f27ecd4fffa35a2486993d245f78faa209
TannerMantooth <137127335+TannerMantooth@users.noreply.github.com>
2023-09-20 23:48:42 -0500

    Added reset function to app.js
    Added in a function that resets the game when the start button is clicked.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const startButton = document.getElementById('btn__reset');
 const keyboard = document.querySelectorAll('.key');
 
 startButton.addEventListener('click', e => {
+    reset();
     game = new Game();
     game.startGame();
 })
@@ -15,3 +16,15 @@ keyboard.forEach(button => {
         game.handleInteraction(e.target);
     })
 })
+
+function reset() {
+    const ul = document.querySelector('ul');
+    ul.innerHTML = '';
+    keyboard.forEach(key => {
+        key.disabled = false;
+        key.classList.remove('wrong');
+        key.classList.remove('chosen');
+    });
+    const heartImg = document.querySelectorAll('.tries img');
+    heartImg.forEach(img => img.src = 'images/liveHeart.png');
+}
